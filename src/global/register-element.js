314b68a4965a722d19bb4482e61ad679f05b0bac
张熠 <2876043242@qq.com>
2021-12-12 21:21:33 +0800

    登录静态页面完成

--- src/global/register-element.js
+++ src/global/register-element.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-12 16:59:47
+ * @LastEditTime: 2021-12-12 20:21:08
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vue3-admin\src\global\register-element.js
+ */
 import { App } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,22 +1,88 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-12-12 12:37:59
+ * @LastEditTime: 2021-12-12 21:14:55
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vue3-admin\src\views\login\login.vue
+-->
 <template>
-    <div class="login">Login
-        <el-button>Default</el-button>
-        <el-button type="primary">Primary</el-button>
-        <el-button type="success">Success</el-button>
-        <el-button type="info">Info</el-button>
-        <el-button type="warning">Warning</el-button>
-    </div>
+  <div class="login">
+    <el-form
+      :label-position="labelPosition"
+      label-width="auto"
+      :model="form"
+      :rules="rules"
+      class="form"
+    >
+      <el-form-item label="邮箱:" prop="userName">
+        <el-input v-model="form.userName" />
+      </el-form-item>
+      <el-form-item label="密码:">
+        <el-input v-model="form.password" type="password" />
+      </el-form-item>
+      <el-form-item label="验证码:">
+        <el-row :gutter="14">
+          <el-col :span="15">
+            <el-input v-model="form.code" />
+          </el-col>
+          <el-col :span="7">
+            <el-button type="success">获取验证码</el-button>
+          </el-col>
+        </el-row>
+      </el-form-item>
+    </el-form>
+    <el-button type="primary" class="login-btn">登录</el-button>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "login"
-    }
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+    const labelPosition = "right";
+    const form = {
+      userName: "",
+      password: "",
+      code: "",
+    };
+    const rules = {
+      userName: [
+        {
+          required: true,
+          message: "请输入正确的邮箱地址",
+          trigger: "blur",
+        },
+      ],
+    };
+    return {
+      labelPosition,
+      form,
+      rules,
+    };
+  },
+});
 </script>
 
-<style scoped lang="scss">
+
+<style lang="scss">
 .login {
-    height: 100vh;
-    background-color: #344a5f;
+  height: 100%;
+  background-color: #344a5f;
+}
+
+.form {
+  width: 60vh;
+  margin: 0 auto;
+  margin-top: 100px;
+}
+.login-btn {
+  width: 24%;
+  display: block;
+  margin-left: 40%;
+}
+.el-form-item__label {
+  color: #fff;
 }
 </style>
